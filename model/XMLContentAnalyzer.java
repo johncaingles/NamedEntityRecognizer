6038477deb23f2fa444683941c442b5837a9707e
kingstonkoa <kingston_koa@yahoo.com>
2015-09-20 13:25:55 +0800

    people entity

--- model/XMLContentAnalyzer.java
+++ model/XMLContentAnalyzer.java
@@ -5,10 +5,9 @@ import java.util.regex.Pattern;
 public class XMLContentAnalyzer {
 	
 	public void analyze(XMLFileContents xmlFile, ExtractedInfo extractedInfo) {
-		// TODO REGEX HERE
-             // String to be scanned to find the pattern.
-      String line = "puta ni Emilio Aguinaldo Arroyo at ni John Ganda at ni Kingston";
-      String pattern = "ni ([A-Z][a-z]*( [A-Z][a-z]*)?( [A-Z][a-z]*)?)";
+      /**ni x*/
+      String line = xmlFile.getBodies().get(9);
+      String pattern = "[n|N]i ([A-Z][a-z]*( [A-Z][a-z]*)?( [A-Z][a-z]*)?)";
 
       // Create a Pattern object
       Pattern r = Pattern.compile(pattern);
@@ -18,7 +17,52 @@ public class XMLContentAnalyzer {
       while (m.find( )) {
          System.out.println("Found value: " + m.group(1) );
       }
-		
+         System.out.println("------------------------------");
+         
+      /**nina x at y*/      
+      line = xmlFile.getBodies().get(9);;
+      pattern = "[n|N]ina ([A-Z][a-z]*( [A-Z][a-z]*)?( [A-Z][a-z]*)?) (at) ([A-Z][a-z]*( [A-Z][a-z]*)?( [A-Z][a-z]*)?)";
+
+      // Create a Pattern object
+      r = Pattern.compile(pattern);
+
+      // Now create matcher object.
+      m = r.matcher(line);
+      while (m.find( )) {
+         System.out.println("Found value: " + m.group(1) );
+         System.out.println("Found value: " + m.group(5) );
+      }
+         System.out.println("------------------------------");	
+         
+         /**si x*/
+      line = xmlFile.getBodies().get(9);;
+      pattern = "[s|S]i ([A-Z][a-z]*( [A-Z][a-z]*)?( [A-Z][a-z]*)?)";
+
+      // Create a Pattern object
+      r = Pattern.compile(pattern);
+
+      // Now create matcher object.
+      m = r.matcher(line);
+      while (m.find( )) {
+         System.out.println("Found value: " + m.group(1) );
+      }
+         System.out.println("------------------------------");
+         
+     /**mag-asawang x at y*/      
+      line = xmlFile.getBodies().get(9);;
+      pattern = "[m|M]ag-asawang ([A-Z][a-z]*( [A-Z][a-z]*)?( [A-Z][a-z]*)?) (at) ([A-Z][a-z]*( [A-Z][a-z]*)?( [A-Z][a-z]*)?)";
+
+      // Create a Pattern object
+      r = Pattern.compile(pattern);
+
+      // Now create matcher object.
+      m = r.matcher(line);
+      while (m.find( )) {
+         System.out.println("Found value: " + m.group(1) );
+         System.out.println("Found value: " + m.group(5) );
+      }
+         System.out.println("------------------------------");	
 	}
+        
 
 }
